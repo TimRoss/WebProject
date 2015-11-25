81c5613f6fed66ae89a02875fa1f054cf8d68e48
Tim Ross <tross@mines.edu>
2015-11-24 19:27:03 -0700

    updated student info page

--- resources/views/pages/studentInfo.blade.php
+++ resources/views/pages/studentInfo.blade.php
@@ -1,45 +1,59 @@
-@extends('layout.master')
+@extends('app')
 
 @section('content')
-<ul>
-    <li>{{$user->name}}</li>
-    <li>Current Team</li>
-    <li><img href=""></li>
-</ul>
-<hr>
-<div id="classesBox">
-    <h2>Classes</h2>
-    <br>
-    <ul id="classesList"></ul>
-</div>
-<div id="languagesBox">
-    <h2>Language Strength</h2>
-    <br>
-    <table>
-        <tr>
-            <td>C++</td>
-            <td>{{$student->c}}</td>
-        </tr>
-        <tr>
-            <td>Java</td>
-            <td>{{$student->java}}</td>
-        </tr>
-        <tr>
-            <td>Python</td>
-            <td>{{$student->python}}</td>
-        </tr>
-    </table>
-</div>
-<div id="membersBox">
-    <h2>Requested Team Members</h2>
-    <br>
-    <ul id="membersList">
-        <li>{{$student->requestedTeamMember1}}</li>
-        <li>{{$student->requestedTeamMember2}}</li>
-    </ul>
-</div>
-<div id="otherBox">
-    <span>Style: {{$student->teamStyle}}</span>
-    <a href="{{$user->id}}/edit"><button >Edit Profile</button></a>
-</div>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Student Info Page</h1>
+            <ul>
+                <li>Student Name</li>
+                <li>Current Team</li>
+                <li><img href=""></li>
+            </ul>
+        </div>
+        <div class="row">
+        <div id="classesBox" class="col-lg-4">
+            <h2 class="text-center">Classes</h2>
+            <br>
+            <ul id="classesList"></ul>
+        </div>
+        <div id="languagesBox" class="col-lg-4">
+            <h2 class="text-center">Language Strength</h2>
+            <br>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Language</th>
+                            <th>Strength</th>
+                        </tr>
+                    </thead>
+                    <tr>
+                        <td>C++</td>
+                        <td>C++ strength</td>
+                    </tr>
+                    <tr>
+                        <td>Java</td>
+                        <td>Java strength</td>
+                    </tr>
+                    <tr>
+                        <td>Python</td>
+                        <td>Python Strength</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div id="membersBox" class="col-lg-4">
+            <h2 class="text-center">Requested Team Members</h2>
+            <br>
+            <ul id="membersList"></ul>
+        </div>
+            </div>
+        <div id="otherBox" class="col-lg-12">
+            <span>Style: </span>
+        </div>
+        <br>
+        <hr>
+        <a href="pages/editInfo/{id}" class="btn btn-info" role="button">Edit Profile</a>
+    </div>
+
 @stop
