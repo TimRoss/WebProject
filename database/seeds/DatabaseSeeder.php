11bc453074ff4a407125273b1728bcb8a5b147bc
Tim Ross <tross@mines.edu>
2015-11-24 18:51:57 -0700

    seeder functional

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,21 +1,63 @@
 <?php
-
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades;
+use App\User;
+use App\student;
+
 use Illuminate\Database\Eloquent\Model;
+
+use League\Csv\Reader;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
-        Model::unguard();
+        $this->call('UserTableSeeder');
+        $this->command->info('User table seeded');
 
-        // $this->call(UserTableSeeder::class);
+        $this->call('StudentTableSeeder');
+        $this->command->info('Student table seeded');
 
-        Model::reguard();
+
     }
+}
+
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+
+        DB::table('users')->truncate();
+        $skip = 0;
+        $reader = Reader::createFromPath(base_path().'/database/seeds/students.csv');
+        foreach($reader as $index => $row){
+            if($skip) {
+                User::create(['name' => $row[0], 'password' => Hash::make($row[2]), 'email' => $row[3]]);
+            }
+            else{
+                $skip = 1;
+            }
+        }
+    }
+}
+
+class StudentTableSeeder extends Seeder{
+    public function run()
+    {
+
+        DB::table('students')->truncate();
+        $skip = 0;
+        $reader = Reader::createFromPath(base_path().'/database/seeds/students.csv');
+        foreach($reader as $index => $row){
+            if($skip) {
+                student::create(['teamStyle' => $row[4], 'c' => $row[5], 'java' => $row[6], 'python'=> $row[7]]);
+            }
+            else{
+                $skip = 1;
+            }
+        }
+    }
+
 }
